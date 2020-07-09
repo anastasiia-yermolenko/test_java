4f54af6cc2bf99a4c497736db87229a53c1cd926
anastasiiayermolenko <agnipova24@gmail.com>
2020-07-09 14:19:03 +0300

    second lesson about System.out.println

--- main/java/com/Firssclass.java
+++ main/java/com/Firssclass.java
@@ -2,9 +2,10 @@ package com;
 
 public class Firssclass {
 
-        public static void main (String[] args) throws java.lang.Exception
-        {
-            System.out.println ("Hello, World!");
-        }
+    public static void main(String[] args) throws java.lang.Exception {
+        System.out.println("Nastena");
+        System.out.println("The");
+        System.out.println("Best");
 
+    }
 }
