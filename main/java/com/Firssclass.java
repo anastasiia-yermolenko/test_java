138984f43ab138c10e637d79dadd06ca2f0d25ca
anastasiiayermolenko <agnipova24@gmail.com>
2020-07-09 14:27:54 +0300

    fourth lesson

--- main/java/com/Firssclass.java
+++ main/java/com/Firssclass.java
@@ -5,9 +5,8 @@ public class Firssclass {
     public static void main(String[] args) throws java.lang.Exception {
 
         {
-            System.out.print("Nastena ");
-            System.out.print("The ");
-            System.out.print("Best");
+            System.out.println("Nas"+"tena");
 
         }
-    }}
+    }
+}
