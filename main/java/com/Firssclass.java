e34353fe0e25af8759e07f6ee93c061f2b61e13d
anastasiiayermolenko <agnipova24@gmail.com>
2020-07-09 14:10:45 +0300

    first lesson about HelloWorld

---
+++ main/java/com/Firssclass.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Firssclass {
+
+        public static void main (String[] args) throws java.lang.Exception
+        {
+            System.out.println ("Hello, World!");
+        }
+
+}
