cb49fd72ebd169b275b054971e93b8ef68185561
anastasiiayermolenko <agnipova24@gmail.com>
2020-07-09 14:51:11 +0300

    9th lesson

--- main/java/com/Firssclass.java
+++ main/java/com/Firssclass.java
@@ -6,9 +6,13 @@ public class Firssclass {
 
         {
             {
-                String nastena;
-                int thirty;
-                String kiev;
+                 /*
+        Сейчас мы выведем на экран фразу 'Nastena Is The Best'
+        */
+                System.out.print("Nastena ");
+                System.out.print("Is ");
+                System.out.print("The ");
+                System.out.print("Best");
 
             }
         }
